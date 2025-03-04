7f6e8d6d6c8e1d30fb9625e391bb6067ddbd0d85
ChaGyoungtae <village1031@gmail.com>
2025-03-04 16:52:58 +0900

    [feature] 나만의 장소 안에 사진 추가, 삭제, 대표 사진 변경기능 구현

---
+++ src/main/java/com/maptist/mappride/mappride/photo/dto/PhotoAddRequestDto.java
@@ -0,0 +1,30 @@
+package com.maptist.mappride.mappride.photo.dto;
+
+import com.maptist.mappride.mappride.member.Member;
+import com.maptist.mappride.mappride.photo.Photo;
+import com.maptist.mappride.mappride.place.Place;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class PhotoAddRequestDto {
+
+    private final Long placeId;
+
+    private final List<MultipartFile> multipartFiles;
+
+
+    public Photo addPhoto(Member member, Place place, String photoUrl){
+        return Photo.builder()
+                .member(member)
+                .place(place)
+                .thumbnail(false)
+                .photoUrl(photoUrl)
+                .build();
+    }
+
+}
