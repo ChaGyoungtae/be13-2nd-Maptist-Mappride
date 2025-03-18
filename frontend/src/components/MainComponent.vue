a70dbc2ea6f385011dad5292d42200f5e28e5a44
ChaGyoungtae <village1031@gmail.com>
2025-03-18 22:56:12 +0900

    backend와 frontend 디렉토리 생성 프론트 정적 페이지 1차 완성

---
+++ frontend/src/components/MainComponent.vue
@@ -0,0 +1,300 @@
+<template>
+  <div class="div">
+    <img class="image-6" src="/src/assets/images/public/image-60.png" />
+    <div class="mappride">Mappride</div>
+    <div class="rectangle-29"></div>
+    <div class="my-categories">My categories</div>
+    <div class="my-page">My page</div>
+    <div class="map">Map</div>
+    <div class="rectangle-28"></div>
+    <img class="image-12" src="/src/assets/images/public/image-120.png" />
+    <img class="image-13" src="/src/assets/images/public/image-130.png" />
+    <img class="image-14" src="/src/assets/images/public/image-140.png" />
+    <div class="rectangle-4"></div>
+    <div class="rectangle-5"></div>
+    <div class="rectangle-6"></div>
+    <div class="address">address</div>
+    <div class="rectangle-7"></div>
+    <div class="user">user</div>
+    <div class="rectangle-8"></div>
+    <img class="rectangle-9" src="/src/assets/images/public/rectangle-90.png" />
+    <div class="search">search</div>
+    <img class="polygon-6" src="/src/assets/images/public/polygon-60.png" />
+    <div class="logout">logout</div>
+    <img class="image-16" src="/src/assets/images/public/image-150.png" />
+  </div>
+</template>
+<script>
+export default {
+  name: "Component",
+  components: {},
+  props: {},
+  data() {},
+};
+</script>
+<style scoped>
+.div,
+.div * {
+  box-sizing: border-box;
+}
+.div {
+  background: #ffffff;
+  height: 1080px;
+  position: relative;
+  overflow: hidden;
+}
+.image-6 {
+  width: 1595px;
+  height: 998px;
+  position: absolute;
+  left: 338px;
+  top: 82px;
+  object-fit: cover;
+}
+.group-5 {
+  position: absolute;
+  inset: 0;
+}
+.mappride {
+  color: #000000;
+  text-align: left;
+  font-family: "Stylish-Regular", sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+  position: absolute;
+  left: 78.27px;
+  top: 31.64px;
+  width: 186.84px;
+  height: 40.08px;
+  -webkit-text-stroke: 1px #ffffff;
+}
+.rectangle-29 {
+  background: #f7f7f7;
+  border-style: solid;
+  border-color: #ffffff;
+  border-width: 0px 1px 0px 0px;
+  width: 338.73px;
+  height: 84.38px;
+  position: absolute;
+  left: -1px;
+  top: 346px;
+}
+.my-categories {
+  color: #000000;
+  text-align: center;
+  font-family: "Stylish-Regular", sans-serif;
+  font-size: 25px;
+  font-weight: 400;
+  position: absolute;
+  left: 46.98px;
+  top: 494.65px;
+  width: 293.98px;
+  height: 104.41px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  -webkit-text-stroke: 1px #ffffff;
+}
+.my-page {
+  color: #000000;
+  text-align: center;
+  font-family: "Stylish-Regular", sans-serif;
+  font-size: 25px;
+  font-weight: 400;
+  position: absolute;
+  left: 20.14px;
+  top: 419.77px;
+  width: 318.14px;
+  height: 104.41px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  -webkit-text-stroke: 1px #ffffff;
+}
+.map {
+  color: #000000;
+  text-align: center;
+  font-family: "Stylish-Regular", sans-serif;
+  font-size: 25px;
+  font-weight: 400;
+  position: absolute;
+  left: 20.14px;
+  top: 335.39px;
+  width: 318.14px;
+  height: 104.41px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  -webkit-text-stroke: 1px #ffffff;
+}
+.rectangle-28 {
+  background: rgba(255, 255, 255, 0);
+  border-style: solid;
+  border-color: #d9d9d9;
+  border-width: 1px;
+  width: 339.62px;
+  height: 1080px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+}
+.image-12 {
+  width: 44.02px;
+  height: 44.02px;
+  position: absolute;
+  left: 25.64px;
+  top: 365.59px;
+  object-fit: cover;
+  aspect-ratio: 1;
+}
+.image-13 {
+  width: 29.75px;
+  height: 29.75px;
+  position: absolute;
+  left: 32.78px;
+  top: 454.99px;
+  object-fit: cover;
+  aspect-ratio: 1;
+}
+.image-14 {
+  width: 30.94px;
+  height: 30.94px;
+  position: absolute;
+  left: 32.86px;
+  top: 531.91px;
+  object-fit: cover;
+  aspect-ratio: 1;
+}
+.group-9 {
+  position: absolute;
+  inset: 0;
+}
+.rectangle-4 {
+  background: #ffffff;
+  width: 1596px;
+  height: 91px;
+  position: absolute;
+  left: 337px;
+  top: 0px;
+}
+.rectangle-5 {
+  background: #ffffff;
+  border-style: solid;
+  border-color: #d2d2d2;
+  border-width: 1px;
+  width: 460px;
+  height: 46px;
+  position: absolute;
+  left: 752px;
+  top: 26px;
+}
+.rectangle-6 {
+  background: #ffffff;
+  border-style: solid;
+  border-color: #d2d2d2;
+  border-width: 1px;
+  width: 165px;
+  height: 45px;
+  position: absolute;
+  left: 568px;
+  top: 27px;
+}
+.address {
+  color: #000000;
+  text-align: left;
+  font-family: "Stylish-Regular", sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  position: absolute;
+  left: 604px;
+  top: 36px;
+  width: 82px;
+  height: 26px;
+  -webkit-text-stroke: 1px #d2d2d2;
+}
+.rectangle-7 {
+  background: #ffffff;
+  border-style: solid;
+  border-color: #d2d2d2;
+  border-width: 1px;
+  width: 165px;
+  height: 44px;
+  position: absolute;
+  left: 568px;
+  top: 72px;
+}
+.user {
+  color: #000000;
+  text-align: left;
+  font-family: "Stylish-Regular", sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  position: absolute;
+  left: 623px;
+  top: 81px;
+  width: 45px;
+  height: 27px;
+  -webkit-text-stroke: 1px #d2d2d2;
+}
+.rectangle-8 {
+  background: #ffffff;
+  border-style: solid;
+  border-color: #d2d2d2;
+  border-width: 1px;
+  width: 126px;
+  height: 45px;
+  position: absolute;
+  left: 1239px;
+  top: 27px;
+}
+.rectangle-9 {
+  width: 126px;
+  height: 45px;
+  position: absolute;
+  left: 1774px;
+  top: 27px;
+  overflow: visible;
+}
+.search {
+  color: #000000;
+  text-align: left;
+  font-family: "Stylish-Regular", sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  position: absolute;
+  left: 1267px;
+  top: 36px;
+  width: 70px;
+  height: 26px;
+  -webkit-text-stroke: 1px #d2d2d2;
+}
+.polygon-6 {
+  width: 23px;
+  height: 15px;
+  position: absolute;
+  left: 714.09px;
+  top: 56px;
+  overflow: visible;
+}
+.logout {
+  color: #000000;
+  text-align: left;
+  font-family: "Stylish-Regular", sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  position: absolute;
+  left: 1804px;
+  top: 36px;
+  width: 67px;
+  height: 26px;
+}
+.image-16 {
+  width: 46px;
+  height: 46px;
+  position: absolute;
+  left: 1633px;
+  top: 18px;
+  object-fit: cover;
+  aspect-ratio: 1;
+}
+</style>
