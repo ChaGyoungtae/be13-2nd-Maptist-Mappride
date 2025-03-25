7066ab53353256bb5d215d6fff10c7fd8da0c540
ChaGyoungtae <village1031@gmail.com>
2025-03-25 17:27:30 +0900

    카테고리 id 로 place list 가져오는 로직 구현

--- backend/src/main/java/com/maptist/mappride/mappride/place/dto/PlacesByCategoryResponseDto.java
+++ backend/src/main/java/com/maptist/mappride/mappride/place/dto/PlacesByCategoryResponseDto.java
@@ -3,10 +3,19 @@ package com.maptist.mappride.mappride.place.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @RequiredArgsConstructor
 @Getter
 public class PlacesByCategoryResponseDto {
 
     private final Long placeId;
+    private final Long categoryId;
     private final String name;
+    private final Double latitude;
+    private final Double longitude;
+    private final String address;
+    private final String color;
+    private final String content;
+    private final LocalDateTime reg_date;
 }
